eb53a6f1657f1c798031aa6736aa04000b584355
tmori3y2 <tmori3y2@gmail.com>
2016-05-07 11:09:01 +0900

    Article_20160507

---
+++ WPFSamples/Typography/PlotData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typography
+{
+    public class PlotData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public decimal X
+        {
+            get { return _X;  }
+            set
+            {
+                if (_X == value) return;
+                _X = value;
+                var handler = PropertyChanged;
+                handler?.Invoke(this, new PropertyChangedEventArgs(nameof(X)));
+            }
+        }
+        private decimal _X;
+
+        public decimal Y
+        {
+            get { return _Y; }
+            set
+            {
+                if (_Y == value) return;
+                _Y = value;
+                var handler = PropertyChanged;
+                handler?.Invoke(this, new PropertyChangedEventArgs(nameof(Y)));
+            }
+        }
+        private decimal _Y;
+    }
+}
