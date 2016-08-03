3d143f780fd5db511386b100a577a849531f7afd
tmori3y2 <tmori3y2@gmail.com>
2016-08-04 01:21:55 +0900

    Change project settings Add targets file

---
+++ NuGetPackages/CultureHelper/CultureHelper/GlobalSuppressions.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="tmori3y2">
+// Copyright (c) 2016 tmori3y2. All rights reserved.
+// </copyright>
+// <author>tmori3y2</author>
+// <date>2016/08/04</date>
+// <summary>Implements the global suppressions class</summary>
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the
+// Code Analysis results, point to "Suppress Message", and click
+// "In Suppression File".
+// You do not need to add suppressions to this file manually.
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification = "Pending")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text must match", Justification = "Pending")]
--- NuGetPackages/CultureHelper/CultureHelper/Properties/AssemblyInfo.cs
+++ NuGetPackages/CultureHelper/CultureHelper/Properties/AssemblyInfo.cs
@@ -1,9 +1,16 @@
-﻿using System.Resources;
+﻿// <copyright file="AssemblyInfo.cs" company="tmori3y2">
+// Copyright (c) 2016 tmori3y2. All rights reserved.
+// </copyright>
+// <author>tmori3y2</author>
+// <date>2016/08/04</date>
+// <summary>Implements the assembly information class</summary>
+
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CultureHelper")]
@@ -19,11 +26,11 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
